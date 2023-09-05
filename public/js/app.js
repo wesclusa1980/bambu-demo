69507a369ee3c1e5294abcef3f7e0c53281d8f81
wesclusa1980 <53847813+wesclusa1980@users.noreply.github.com>
2023-09-05 12:19:25 -0400

    x

--- public/js/app.js
+++ public/js/app.js
@@ -9,37 +9,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-document.addEventListener("DOMContentLoaded", function() {
-  // Listen for click event on open-modal-button elements
-  var openModalButtons = document.querySelectorAll(".open-modal-button");
-  openModalButtons.forEach(function(button) {
-      button.addEventListener("click", function(event) {
-          event.preventDefault(); // Prevent default link behavior
-
-          // Get the ID of the modal from the data attribute
-          var modalId = button.getAttribute("data-modal-id");
-
-          // Show the modal with the specified ID
-          var modal = document.getElementById(modalId);
-          modal.style.display = "block";
-      });
-  });
-
-  // Listen for click event on close buttons inside the modals
-  var closeButtons = document.querySelectorAll(".close");
-  closeButtons.forEach(function(button) {
-      button.addEventListener("click", function() {
-          // Hide the modal containing the close button
-          var modal = button.closest(".modal");
-          modal.style.display = "none";
-      });
-  });
-
-  // Listen for click event on window to close modal when clicking outside
-  window.addEventListener("click", function(event) {
-      if (event.target.classList.contains("modal")) {
-          event.target.style.display = "none";
-      }
-  });
-});
-
