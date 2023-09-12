4a08accd2729c34e326bf362c90a0c143e7332e8
wesclusa1980 <53847813+wesclusa1980@users.noreply.github.com>
2023-09-12 11:22:53 -0400

    x

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Use CORS middleware
+app.use(cors());
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,5 +15,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-// Use CORS middleware
-app.use(cors());
